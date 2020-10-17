3d557b7dbfc50d39f66af54acb10a084020f211e
Abraham E. Tavarez <abetavarez@icloud.com>
2020-10-17 12:50:52 -0400

    PUT route

--- server.js
+++ server.js
@@ -3,23 +3,32 @@ const {
   getProducts,
   getProduct,
   createProduct,
+  updateProduct,
 } = require('./controllers/product');
 
 const server = http.createServer((req, res) => {
   //* checks url and method from request
   if (req.url === '/api/products' && req.method === 'GET') {
-    //* takes both req and res objects
+    // takes both req and res objects
     getProducts(req, res); //gets all products
   } else if (
-    //*regex extracts id if it matches the url with any number
+    //regex extracts id if it matches the url with any number
     req.url.match(/\/api\/products\/([0-9]+)/) &&
     req.method === 'GET'
   ) {
-    //* extracts id from url
+    // extracts id from url = api/products/id
     const id = req.url.split('/')[3];
     getProduct(req, res, id); //gets single product
   } else if (req.url === '/api/products' && req.method === 'POST') {
     createProduct(req, res);
+  } else if (
+    //regex extracts id if it matches the url with any number
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === 'PUT'
+  ) {
+    // split api/products/id
+    const id = req.url.split('/')[3];
+    updateProduct(req, res, id);
   } else {
     //* 404 Not Found
     res.writeHead(404, { 'Content-Type': 'application/json' });
